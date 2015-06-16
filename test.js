cb73769432290d4fbf79a1015128c5181ad0f7dc
Aria Stewart <aredridel@dinhe.net>
2015-06-16 12:09:59 -0400

    Move from tape to tap

--- test.js
+++ test.js
@@ -1,44 +1,46 @@
 var copyBrowserTo = require('./');
 
-var test = require('tape');
+var test = require('tap').test;
 var path = require('path');
 var rimraf = require('rimraf');
 var rsvp = require('rsvp');
 var fs = require('fs');
 
-test('trivial', function (t) {
-    copyBrowserTo(path.resolve(__dirname, 'test-fixtures/trivial'), 'tmp').then(function (d) {
-        t.ok(d);
-        return checkExists('tmp');
-    }).catch(t.error).finally(cleanup).finally(t.end);
-});
+test('copy-browser-modules', function (m) {
+    m.test('trivial', function (t) {
+        copyBrowserTo(path.resolve(__dirname, 'test-fixtures/trivial'), 'tmp').then(function (d) {
+            t.ok(d);
+            return checkExists('tmp');
+        }).catch(t.error).finally(cleanup).finally(t.end);
+    });
 
-test('nested', function (t) {
-    copyBrowserTo(path.resolve(__dirname, 'test-fixtures/nested'), 'tmp').then(function (d) {
-        t.ok(d);
-        return checkExists('tmp');
-    }).catch(t.error).finally(cleanup).finally(t.end);
-});
+    m.test('nested', function (t) {
+        copyBrowserTo(path.resolve(__dirname, 'test-fixtures/nested'), 'tmp').then(function (d) {
+            t.ok(d);
+            return checkExists('tmp');
+        }).catch(t.error).finally(cleanup).finally(t.end);
+    });
 
-test('irrelevant modules excluded', function (t) {
-    copyBrowserTo(path.resolve(__dirname, 'test-fixtures/irrelevant'), 'tmp').then(function (d) {
-        t.ok(d);
-        return checkNotExists('tmp/irrelevant1');
-    }).catch(t.error).finally(cleanup).finally(t.end);
-});
+    m.test('irrelevant modules excluded', function (t) {
+        copyBrowserTo(path.resolve(__dirname, 'test-fixtures/irrelevant'), 'tmp').then(function (d) {
+            t.ok(d);
+            return checkNotExists('tmp/irrelevant1');
+        }).catch(t.error).finally(cleanup).finally(t.end);
+    });
 
-test('overrides work', function (t) {
-    copyBrowserTo(path.resolve(__dirname, 'test-fixtures/override'), 'tmp').then(function (d) {
-        t.ok(d);
-        return checkExists('tmp/component1');
-    }).catch(t.error).finally(cleanup).finally(t.end);
-});
+    m.test('overrides work', function (t) {
+        copyBrowserTo(path.resolve(__dirname, 'test-fixtures/override'), 'tmp').then(function (d) {
+            t.ok(d);
+            return checkExists('tmp/component1');
+        }).catch(t.error).finally(cleanup).finally(t.end);
+    });
 
-test('overlap', function (t) {
-    copyBrowserTo(path.resolve(__dirname, 'test-fixtures/overlap'), 'tmp').then(t.fail).catch(function (e) {
-        t.ok(e);
-        t.equal(e.message, 'Overlapping packages found: component1');
-    }).finally(cleanup).finally(t.end);
+    m.test('overlap', function (t) {
+        copyBrowserTo(path.resolve(__dirname, 'test-fixtures/overlap'), 'tmp').then(t.fail).catch(function (e) {
+            t.ok(e);
+            t.equal(e.message, 'Overlapping packages found: component1');
+        }).finally(cleanup).finally(t.end);
+    });
 });
 
 function cleanup() {
